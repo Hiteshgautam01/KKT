409e5a980d2f894decf51ebeedc2957922048123
Hitesh Gautam <gautam31.hitesh@gmail.com>
2018-02-02 23:29:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hitesh/testing2/FragmentOne.java
@@ -0,0 +1,26 @@
+package com.example.hitesh.testing2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hitesh on 10/01/18.
+ */
+
+public class FragmentOne extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_one, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/hitesh/testing2/FragmentTwo.java
@@ -0,0 +1,27 @@
+package com.example.hitesh.testing2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hitesh on 10/01/18.
+ */
+
+public class FragmentTwo extends Fragment
+{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_two, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
